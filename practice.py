e99ae8a3e882a390b68b620b6f4cf4abbd982bdf
lahatjaella <lahatjaella@gmail.com>
2024-03-26 11:50:15 -0400

    testing pushing to main

---
+++ practice.py
@@ -0,0 +1,18 @@
+# printing to terminal
+print("Hello, World!")
+
+#MVP minimum viable product
+#user being able to enter a brand and return a rating to the user
+
+#getting user input
+name = input("Enter your name:\n")
+
+#welcome user with own name
+print("Hello " + name)
+
+
+
+
+
+
+
